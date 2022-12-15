2c555572421a62335142ee553a55dc97f1063a92
vamsi krishna <vamsi.bandaru.22.1@mountblue.tech>
2022-12-15 11:32:29 +0530

    modified to make the make file execute itself. added file paths and changed the output check file from index.cjs

--- src/server/4-top-10-economical-bowlers-in-the-year.cjs
+++ src/server/4-top-10-economical-bowlers-in-the-year.cjs
@@ -1,46 +1,63 @@
-function top10EconomicalBowlersInyear(matchesData = [], deliveriesData = [], year) {
-  if (Number(year) && Array.isArray(matchesData) && Array.isArray(deliveriesData)) {
-    // Stored all Ids in the year 2015
-    const yearIds = []
-    matchesData.map((matches) => {
-      if (matches.season == year) {
-        yearIds.push(matches.id)
-      }
-    })
-    // using the yearIds deliveriesData gathered
-    const playersEconomyData = deliveriesData
-      .filter(delivery => yearIds.includes(delivery.match_id))
-      .reduce((output, deliveries) => {
+const fs = require("fs");
+const csv = require('csvtojson')
+const matches = '../data/matches.csv'
+const deliveries = '../data/deliveries.csv'
 
-        if (output[deliveries.bowler] && output[deliveries.bowler]['total_balls'] && output[deliveries.bowler]['total_runs']) {
+csv()
+  .fromFile(matches)
+  .then((matchesData) => {
+    csv()
+      .fromFile(deliveries)
+      .then((deliveriesData) => {
+        try {
+          function top10EconomicalBowlersInyear(matchesData = [], deliveriesData = [], year) {
+            if (Number(year) && Array.isArray(matchesData) && Array.isArray(deliveriesData)) {
+              // Stored all Ids in the year 2015
+              const yearIds = []
+              matchesData.map((matches) => {
+                if (matches.season == year) {
+                  yearIds.push(matches.id)
+                }
+              })
+              // using the yearIds deliveriesData gathered
+              const playersEconomyData = deliveriesData
+                .filter(delivery => yearIds.includes(delivery.match_id))
+                .reduce((output, deliveries) => {
 
-          output[deliveries.bowler]['total_balls'] += 1
-          output[deliveries.bowler]['total_runs'] += Number(deliveries.total_runs)
-          output[deliveries.bowler]['economy'] = Number((output[deliveries.bowler]['total_runs'] / (output[deliveries.bowler]['total_balls'] / 6)).toFixed(2))
+                  if (output[deliveries.bowler] && output[deliveries.bowler]['total_balls'] && output[deliveries.bowler]['total_runs']) {
 
-        } else {
-          output[deliveries.bowler] = {}
-          output[deliveries.bowler]['total_balls'] = Number(deliveries.ball)
-          output[deliveries.bowler]['total_runs'] = Number(deliveries.total_runs)
+                    output[deliveries.bowler]['total_balls'] += 1
+                    output[deliveries.bowler]['total_runs'] += Number(deliveries.total_runs)
+                    output[deliveries.bowler]['economy'] = Number((output[deliveries.bowler]['total_runs'] / (output[deliveries.bowler]['total_balls'] / 6)).toFixed(2))
+
+                  } else {
+                    output[deliveries.bowler] = {}
+                    output[deliveries.bowler]['total_balls'] = Number(deliveries.ball)
+                    output[deliveries.bowler]['total_runs'] = Number(deliveries.total_runs)
+                  }
+                  return output
+                }, {})
+
+              const sortedPlayersEconomyData = Object.entries(playersEconomyData).sort(([a, { economy: bowler1 }], [b, { economy: bowler2 }]) => {
+                return bowler1 > bowler2 ? 1 : -1;
+              });
+
+              const sortedBowlersData = sortedPlayersEconomyData.reduce((player, each) => {
+                let bowlerData = {}
+                bowlerData['bowler_name'] = each[0]
+                bowlerData['economy'] = each[1].economy
+                player.push(bowlerData)
+                return player
+              }, [])
+
+              return sortedBowlersData.slice(0, 10)
+            }
+            return [];
+          } 
+          let top10EconomicalBowlersInyearOutputData = top10EconomicalBowlersInyear(matchesData, deliveriesData, 2015)
+          fs.writeFileSync('../public/output/4-top-10-economical-bowlers-in-the-year.json', JSON.stringify(top10EconomicalBowlersInyearOutputData))
+        } catch (err) {
+          console.log(err);
         }
-        return output
-      }, {})
-
-    const sortedPlayersEconomyData = Object.entries(playersEconomyData).sort(([a, { economy: bowler1 }], [b, { economy: bowler2 }]) => {
-      return bowler1 > bowler2 ? 1 : -1;
-    });
-
-    const sortedBowlersData = sortedPlayersEconomyData.reduce((player, each) => {
-      let bowlerData = {}
-      bowlerData['bowler_name'] = each[0]
-      bowlerData['economy'] = each[1].economy
-      player.push(bowlerData)
-      return player
-    }, [])
-
-    return sortedBowlersData.slice(0, 10)
-  }
-  return [];
-}
-
-module.exports = top10EconomicalBowlersInyear;
+      })
+  })

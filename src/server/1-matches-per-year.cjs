895c096bb861785db98bb70189a89f03489abb53
vamsi krishna <vamsi.bandaru.22.1@mountblue.tech>
2022-12-15 11:07:59 +0530

    written logic to get save the output into json file. without index file need.

--- src/server/1-matches-per-year.cjs
+++ src/server/1-matches-per-year.cjs
@@ -1,17 +1,31 @@
-function matchesPerYear(matchesData = []) {
-    if (Array.isArray(matchesData) && matchesData != undefined) {
+const fs = require("fs");
+const csv = require('csvtojson')
+const matches = '../data/matches.csv'
 
-        let matchesPlayedPerYear = matchesData.reduce((totalMatches, eachItem) => {
-            if (totalMatches[eachItem.season]) {
-                totalMatches[eachItem.season] += 1
-            } else {
-                totalMatches[eachItem.season] = 1
+csv()
+    .fromFile(matches)
+    .then((matchesData) => {
+        try {
+            function matchesPerYear(matchesData = []) {
+                if (Array.isArray(matchesData) && matchesData != undefined) {
+
+                    let matchesPlayedPerYear = matchesData.reduce((totalMatches, eachItem) => {
+                        if (totalMatches[eachItem.season]) {
+                            totalMatches[eachItem.season] += 1
+                        } else {
+                            totalMatches[eachItem.season] = 1
+                        }
+                        return totalMatches
+                    }, {})
+
+                    return matchesPlayedPerYear;
+                }
+                return []
             }
-            return totalMatches
-        }, {})
-        
-        return matchesPlayedPerYear;
-    }
-    return []
-}
-module.exports = matchesPerYear;
+
+            let matchesPerYearOutput = matchesPerYear(matchesData)
+            fs.writeFileSync('../public/output/1-matches-per-year.json', JSON.stringify(matchesPerYearOutput))
+        } catch (err) {
+            console.log(err);
+        }
+    })

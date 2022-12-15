ca3cdf59b92e877264b4fa96e477ffea26b8df17
vamsi krishna <vamsi.bandaru.22.1@mountblue.tech>
2022-12-15 11:15:58 +0530

    modified to make the make file execute itself. changed the output check file from index.cjs

--- src/server/2-matches-won-per-team-per-year.cjs
+++ src/server/2-matches-won-per-team-per-year.cjs
@@ -1,20 +1,33 @@
-function matchesWonPerTeamPerYear(matchesData = []) {
-  let teamWon = {}
-  if (Array.isArray(matchesData) && matchesData != undefined) {
-    matchesData.map((matchesData) => {
+const fs = require("fs");
+const csv = require('csvtojson')
+const matches = '../data/matches.csv'
 
-      if (teamWon[matchesData.season] != undefined && teamWon[matchesData.season][matchesData.winner] != undefined) {
-        teamWon[matchesData.season][matchesData.winner] += 1
+csv()
+  .fromFile(matches)
+  .then((matchesData) => {
+    try {
+      function matchesWonPerTeamPerYear(matchesData = []) {
+        let teamWon = {}
+        if (Array.isArray(matchesData) && matchesData != undefined) {
+          matchesData.map((matchesData) => {
 
-      } else if (teamWon[matchesData.season] == undefined) {
-        teamWon[matchesData.season] = {}
-        teamWon[matchesData.season][matchesData.winner] = 1
+            if (teamWon[matchesData.season] != undefined && teamWon[matchesData.season][matchesData.winner] != undefined) {
+              teamWon[matchesData.season][matchesData.winner] += 1
 
-      } else {
-        teamWon[matchesData.season][matchesData.winner] = 1
+            } else if (teamWon[matchesData.season] == undefined) {
+              teamWon[matchesData.season] = {}
+              teamWon[matchesData.season][matchesData.winner] = 1
+
+            } else {
+              teamWon[matchesData.season][matchesData.winner] = 1
+            }
+          })
+        }
+        return teamWon;
       }
-    })
-  }
-  return teamWon;
-}
-module.exports = matchesWonPerTeamPerYear;
+      let matchesWonPerTeamPerYearOutputData = matchesWonPerTeamPerYear(matchesData)
+      fs.writeFileSync('../public/output/2-matches-won-per-team-per-year.json', JSON.stringify(matchesWonPerTeamPerYearOutputData))
+    } catch (err) {
+      console.log(err);
+    }
+  })

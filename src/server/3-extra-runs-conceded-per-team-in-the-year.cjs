4e6eb49301623c66f4765632836ac91c8f5b0206
vamsi krishna <vamsi.bandaru.22.1@mountblue.tech>
2022-12-15 11:26:22 +0530

    modified to make the make file execute itself. changed the output check file from index.cjs

--- src/server/3-extra-runs-conceded-per-team-in-the-year.cjs
+++ src/server/3-extra-runs-conceded-per-team-in-the-year.cjs
@@ -1,29 +1,45 @@
-function extraRunsConcededPerTeamInTheYear(matchesData = [], deliveriesData = [], year) {
-  let yearIds = []
-  if (Number(year) && Array.isArray(matchesData) && Array.isArray(deliveriesData)) {
-    // Stored all Ids in the year 2016
-    matchesData.map((matches) => {
-      if (matches.season == year) {
-        yearIds.push(matches.id)
-      }
-    })
-    // using the yearIds deliveriesData gathered
-    const yearDeliveriesData = deliveriesData.filter(delivery => yearIds.includes(delivery.match_id))
+const fs = require("fs");
+const csv = require('csvtojson')
+const matches = '../data/matches.csv'
+const deliveries = '../data/deliveries.csv'
 
-    let result = yearDeliveriesData.reduce((output, deliveries) => {
+csv()
+  .fromFile(matches)
+  .then((matchesData) => {
+    csv()
+      .fromFile(deliveries)
+      .then((deliveriesData) => {
+        try {
+          function extraRunsConcededPerTeamInTheYear(matchesData = [], deliveriesData = [], year) {
+            let yearIds = []
+            if (Number(year) && Array.isArray(matchesData) && Array.isArray(deliveriesData)) {
+              // Stored all Ids in the year 2016
+              matchesData.map((matches) => {
+                if (matches.season == year) {
+                  yearIds.push(matches.id)
+                }
+              })
+              // using the yearIds deliveriesData gathered
+              const yearDeliveriesData = deliveriesData.filter(delivery => yearIds.includes(delivery.match_id))
 
-      if (output[deliveries.bowling_team]) {
-        output[deliveries.bowling_team] += Number(deliveries.extra_runs)
+              let result = yearDeliveriesData.reduce((output, deliveries) => {
 
-      } else {
-        output[deliveries.bowling_team] = Number(deliveries.extra_runs)
-      }
-      return output
-    }, {})
+                if (output[deliveries.bowling_team]) {
+                  output[deliveries.bowling_team] += Number(deliveries.extra_runs)
 
-    return result;
-  }
-  return [];
-}
+                } else {
+                  output[deliveries.bowling_team] = Number(deliveries.extra_runs)
+                }
+                return output
+              }, {})
 
-module.exports = extraRunsConcededPerTeamInTheYear;
+              return result;
+            }
+            return [];
+          } let extraRunsConcededPerTeamInTheYearOutputData = extraRunsConcededPerTeamInTheYear(matchesData, deliveriesData, 2016)
+          fs.writeFileSync('../public/output/3-extra-runs-conceded-per-team-in-the-year.json', JSON.stringify(extraRunsConcededPerTeamInTheYearOutputData))
+        } catch (err) {
+          console.log(err);
+        }
+      })
+  })
